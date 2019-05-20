6c6334f519c367389fd89d64495ed72af4e2fb53
brnpapa <bruno.papa@hotmail.com>
2019-05-20 01:32:28 -0300

    chat client-server ok

--- src/Client.java
+++ src/Client.java
@@ -1,66 +1,76 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
-// Scanner sempre le um InputStream
-// PrintStream sempre escreve num OutputStream
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 class Client {
    public static void main(String[] args) {
+      new Client("127.0.0.1", 8080).init();
       // new Window();
-      try {
-         //dispara cliente
-         new Client("127.0.0.1", 8080).set();
-      }
-      catch(UnknownHostException e) {
-         System.out.println("UnknownHostException: " + e);
-      } 
-      catch(IOException e) {
-         System.out.println("IOException: " + e);
-      }
    }
-
+   
    private String host;
    private int porta;
-
    public Client(String host, int porta) {
       this.host = host;
       this.porta = porta;
    }
-
-   public void set() throws UnknownHostException, IOException {
-      Socket client = new Socket(this.host, this.porta);
-      System.out.println("Conectado ao servidor.");
-
-      //thread que recebe mensagens do servidor
-      new Recebedor(client.getInputStream()).start();
-
-
-      //DUVIDA DO QUE ENVIAR (OBJETO? COMO? NAO SEI MEU)
-      Scanner is = new Scanner(System.in); //lê do teclado
-
-      PrintStream os = new PrintStream(client.getOutputStream());
-      while (is.hasNextLine())
-         os.println(is.nextLine()); //envia para o servidor
-      is.close();   
-      os.close();   
-      client.close();
+   
+   public void init() {
+      try {
+         Socket socket = new Socket(this.host, this.porta);
+         new Receiver(socket).start();
+         new Sender(socket).start();
+      } catch (UnknownHostException e) {
+         System.out.println("UnknownHostException: " + e);
+      } catch (IOException e) {
+         System.out.println("IOException: " + e);
+      }
    }
 }
 
-class Recebedor extends Thread {
-   private InputStream server;
-   public Recebedor(InputStream server) {
-      this.server = server;
+class Sender extends Thread {
+   private Socket socket;
+   public Sender(Socket socket) {
+      this.socket = socket;
    }
    public void run() {
-      Scanner s = new Scanner(this.server); //le do servidor
-      while (s.hasNextLine())
-         System.out.println(s.nextLine()); //imprime na tela
-      s.close();
+      try {
+         PrintStream out = new PrintStream(socket.getOutputStream());
+         Scanner stdIn = new Scanner(System.in);
+         
+         String clientInput;
+         while (stdIn.hasNextLine()) {
+            clientInput = stdIn.nextLine();
+            out.println(clientInput); //envia ao servidor
+         }
+         out.close();
+         stdIn.close();
+         socket.close();
+      } catch(IOException e) {
+         System.out.println("IOException: " + e);
+      }
    }
 }
 
+class Receiver extends Thread {
+   private Socket socket;
+   public Receiver(Socket socket) {
+      this.socket = socket;
+   }
+   public void run() {
+      try {
+         Scanner in = new Scanner(socket.getInputStream());
+         
+         String serverInput;
+         while (in.hasNextLine()) {
+            serverInput = in.nextLine(); //recebe do servidor
+            System.out.println(serverInput);
+         }
+         in.close();
+      } catch(UnknownHostException e) {
+         System.out.println("UnknownHostException: " + e);
+      } catch(IOException e) {
+         System.out.println("IOException: " + e);
+      }
+   }
+}
--- src/Server.java
+++ src/Server.java
@@ -1,68 +1,76 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 //recebe e trata as informações de cada cliente!!!
 //depois, distribui para todos os clientes conectados ao servidor
 class Server {
    public static void main(String[] args) {
+      new Server(8080).init();
+   }
+   private int port;
+   public Server(int port) {
+      this.port = port;
+   }
+
+   public void init() {
       try {
-         //inicia servidor
-         new Server(8080).set();
-      } catch (IOException e) {
+         System.out.println("Iniciando servidor...");
+         ServerSocket serverSocket = new ServerSocket(this.port); //socket escuta a porta
+         System.out.println("Porta " + this.port + " aberta.");
+         
+         //escuta clientes
+         while (true) {
+            System.out.println("Aguardando conexão...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Cliente " + clientSocket.getInetAddress().getHostName() + " se conectou.");
+            new ThreadClient(clientSocket).start();
+         }
+         // System.out.println("Servidor encerrado.");
+      } catch(IOException e) {
          System.out.println("IOException: " + e);
          System.exit(1);
       }
    }
-
-   private int port;
-   private List<PrintStream> listClients;
-
-   public Server(int port) {
-      this.port = port;
-      this.listClients = new ArrayList<PrintStream>();
-   }
-
-   public void set() throws IOException {
-      ServerSocket server = new ServerSocket(this.port);
-      System.out.println("Porta " + this.port + " aberta.");
-
-      while (true) {
-         //aceita um cliente
-         Socket client = server.accept();
-         System.out.println("Cliente " + client.getInetAddress().getHostAddress() + " se conectou.");
-
-         //adiciona saída do cliente na lista
-         this.listClients.add(new PrintStream(client.getOutputStream()));
-
-         //trata o cliente em uma nova thread
-         new ThreadClient(client.getInputStream(), this).start();
-      }
-   }
-
-   public void distributeMessages(String msg) {
-      for (PrintStream client : this.listClients)
-         client.println(msg); //imprime na tela do cliente
-   }
 }
 
 class ThreadClient extends Thread {
-   private InputStream client;
-   private Server server;
-
-   public ThreadClient(InputStream client, Server server) {
-      this.client = client;
-      this.server = server;
+   private Socket clientSocket = null;
+   static List<PrintStream> listClients = new ArrayList<PrintStream>(); //da classe!
+   
+   public ThreadClient(Socket clientSocket) {
+      this.clientSocket = clientSocket;
    }
    public void run() {
-      Scanner s = new Scanner(this.client); //le do cliente, quando chega algo
-      while (s.hasNextLine())
-         server.distributeMessages(s.nextLine()); //envia para todos os cliente
-      s.close();
+      try {
+         //leitor p o fluxo de entrada do cliente
+         Scanner in = new Scanner(clientSocket.getInputStream());
+         //gravador p o fluxo de saída do cliente
+         PrintStream out = new PrintStream(clientSocket.getOutputStream(), true);
+         listClients.add(out);
+         
+         String inputLine, outputLine;
+         while (in.hasNextLine()) { 
+            //FICA NO LOOPING ENQUANTO O CLIENTE TA NO SERVIDOR
+            inputLine = in.nextLine();
+
+            //trata a entrada do cliente 
+            outputLine = inputLine;
+
+            //distribui para todos os clientes
+            for (PrintStream outC : listClients)
+               outC.println(outputLine);
+
+            // if (outputLine.equals(""))
+            //    break;
+         }
+         System.out.println("Cliente " + clientSocket.getInetAddress().getHostName() + " se desconectou.");
+         listClients.remove(out);
+         in.close();
+         out.close();
+         clientSocket.close();
+      } catch(IOException e) {
+         e.printStackTrace();
+      }
    }
 }
